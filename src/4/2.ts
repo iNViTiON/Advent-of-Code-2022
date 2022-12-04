18359b428b98d3315ecbfc8e8456bd2b7a2b3370
iNViTiON <13241662+iNViTiON@users.noreply.github.com>
2022-12-04 13:29:31 +0700

    refactor: day 4

--- src/4/2.ts
+++ src/4/2.ts
@@ -1,22 +1,25 @@
 import { createReadStream } from 'node:fs';
 import { createInterface } from 'node:readline/promises';
-import { count, filter, from, map, mergeMap, range, reduce, toArray } from 'rxjs';
+import type { Observable, UnaryFunction } from 'rxjs';
+import { count, filter, from, map, mergeMap, pipe, range, reduce, toArray } from 'rxjs';
 
-const rl = createInterface({
-  input: createReadStream(`./src/4/${process.argv[2]}.txt`),
-});
+const pairSection$ = from(
+  createInterface({
+    input: createReadStream(`./src/4/${process.argv[2]}.txt`),
+  })
+);
 
-from(rl).pipe(
-  mergeMap(pairSection => from(pairSection.split(',')).pipe(
-    map(section => section.split('-').map(Number)),
-    mergeMap(([start, end]) => range(start, (end - start) + 1).pipe(
-      reduce((sectionSet, section) => sectionSet.add(section), new Set<number>()),
-    )),
-    toArray(),
-    filter(([sectionSetA, sectionSetB]) =>
-      [...sectionSetB].some(section => sectionSetA.has(section))
-      || [...sectionSetA].some(section => sectionSetB.has(section))
-    ),
+const isOverlap = <T>([setA, setB]: Set<T>[]) => new Set<T>([...setA, ...setB]).size !== setA.size + setB.size;
+const filterOnlyContain: UnaryFunction<Observable<string>, Observable<Set<number>[]>> = pipe(
+  map((section: string) => section.split('-').map(Number)),
+  mergeMap(([start, end]) => range(start, (end - start) + 1).pipe(
+    reduce<number, Set<number>>((sectionSet, section) => sectionSet.add(section), new Set<number>())
   )),
+  toArray(),
+  filter(isOverlap),
+);
+
+pairSection$.pipe(
+  mergeMap(pairSection => from(pairSection.split(',')).pipe(filterOnlyContain)),
   count(),
-).subscribe(sumOfPriorities => console.log('Assignment pairs does one range fully contain the other count is', sumOfPriorities));
+).subscribe(sumOfPriorities => console.log('Assignment pairs do the ranges overlap count is', sumOfPriorities));
